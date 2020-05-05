a33caaa47c2faa5412140a564c31f31d15fa5274
Paweł Gruhn <50170421+Pawli98@users.noreply.github.com>
2020-05-05 14:54:45 +0200

    Add files via upload

---
+++ lab9/src/main/java/wizut/tpsi/lab9/BlogController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizut.tpsi.lab9;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author pawel
+ */
+@Controller
+public class BlogController 
+{
+    
+     @Autowired
+    private BlogRepository blogRepository;
+     
+   @RequestMapping("/")
+    public String Home(Model model) throws SQLException{
+        List<BlogPost> posts;
+        posts = blogRepository.getAllPosts();
+
+        model.addAttribute("posts", posts);
+
+        return "index";
+    }
+
+    /**
+     *
+     * @param post
+     * @return
+     * @throws SQLException
+     */
+    @PostMapping("/newpost")
+    public String newPost(BlogPost post) throws SQLException
+    {
+        blogRepository.createPost(post);
+        return "redirect:/";
+    }
+    
+    /**
+     *
+     * @param post
+     * @return
+     * @throws SQLException
+     */
+    @PostMapping("/deletepost")
+    public String deletePost(BlogPost post) throws SQLException 
+    {
+        blogRepository.deletePost(post.getTitle());
+        return "redirect:/";
+    }
+    
+
+}
+
